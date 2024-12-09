75e41c20ad9cfb3d6e75a23f98f1578d4c1743af
vanhieu251206 <vanhieu25122006@gmail.com>
2024-12-09 22:43:36 +0700

    Tạo demo giao diện trang chủ, giao diện trang giỏ hàng

--- Pet_Health_Care_System/app/urls.py
+++ Pet_Health_Care_System/app/urls.py
@@ -3,5 +3,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name = "home"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
 ]
