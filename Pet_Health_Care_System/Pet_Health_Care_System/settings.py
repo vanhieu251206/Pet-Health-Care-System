ca318054cb536b5f1be4088af93423818228af37
vanhieu251206 <vanhieu25122006@gmail.com>
2025-01-22 12:32:53 +0700

    a

--- Pet_Health_Care_System/Pet_Health_Care_System/settings.py
+++ Pet_Health_Care_System/Pet_Health_Care_System/settings.py
@@ -1,9 +1,6 @@
 import os
 from dotenv import load_dotenv
 from pathlib import Path
-import pymysql
-pymysql.install_as_MySQLdb()
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
