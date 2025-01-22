706c90c6ffbf4d69c4c6090b518f39397242fbdd
vanhieu251206 <vanhieu25122006@gmail.com>
2025-01-22 11:46:36 +0700

    a

--- Pet_Health_Care_System/pets/urls.py
+++ Pet_Health_Care_System/pets/urls.py
@@ -3,8 +3,8 @@ from django.shortcuts import redirect
 from . import views
 
 urlpatterns = [
-    path('', lambda request: redirect('index/')),
-    path('index/', views.index, name = "index"),
+    path('', lambda request: redirect('guest_dashboard/')),
+    path('guest_dashboard/', views.guest_dashboard, name = "guest_dashboard"),
     path('login/', views.login, name = "login"),
     path('quenmk/', views.quenmk, name = "quenmk"),
 ]
--- Pet_Health_Care_System/pets/views.py
+++ Pet_Health_Care_System/pets/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
+def guest_dashboard(request):
     context = {}
-    return render(request, 'pets/index.html', context)
+    return render(request, 'pets/guest_dashboard .html', context)
 
 def login(request):
     context = {}
