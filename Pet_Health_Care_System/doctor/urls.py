d3441ecc3ddfb0ba0d2a068815f984151658df6e
DoanLeVinh <vinhdl1636@ut.edu.vn>
2025-02-03 09:32:00 +0700

    update update&Calendar_doctor interface

--- Pet_Health_Care_System/doctor/urls.py
+++ Pet_Health_Care_System/doctor/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('update/', views.update, name='update'),
+    path('Calendar_doctor/', views.Calendar_doctor, name='Calendar_doctor'),
     path('infor_view/', views.infor_view, name='infor_view'),
 ]
--- Pet_Health_Care_System/doctor/views.py
+++ Pet_Health_Care_System/doctor/views.py
@@ -6,3 +6,9 @@ def home(request):
 
 def infor_view(request):
     return render(request, 'infor_view.html')
+
+def update(request):
+    return render(request, 'update.html')
+
+def Calendar_doctor(request):
+    return render(request, 'Calendar_doctor.html')
