c4b452e8aa7b5e1dcf5b600042508510a6acf180
vanhieu251206 <vanhieu25122006@gmail.com>
2025-02-07 15:44:07 +0700

    Thêm giao diện đăng nhập và đăng ký, bao gồm cả phần chọn vai trò. Xử lý backend đăng nhập, xác thực người dùng, kiểm tra vai trò.

--- Pet_Health_Care_System/Pet_Health_Care_System/urls.py
+++ Pet_Health_Care_System/Pet_Health_Care_System/urls.py
@@ -8,3 +8,6 @@ urlpatterns = [
     path('', include('pets.urls')),
     path('staff/', include('staff.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Pet_Health_Care_System/pets/views.py
+++ Pet_Health_Care_System/pets/views.py
@@ -5,8 +5,9 @@ from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from .models import Product
+from staff.views import tong_quan
 
-User = get_user_model()
+CustomUser = get_user_model()
 
 # Create your views here.
 def guest_dashboard(request):
@@ -16,10 +17,6 @@ def guest_dashboard(request):
 def login_page(request):
     context = {}
     return render(request, 'pets/login_page.html', context)
-
-def select_role(request):
-    context = {}
-    return render(request, 'pets/select_role.html', context)
 
 def dashboard_customer(request):
     context = {}
@@ -49,6 +46,10 @@ def contact(request):
     context ={}
     return render(request, 'pets/contact.html', context)
 
+def register_page(request):
+    context ={}
+    return render(request, 'pets/register_page.html', context)
+
 def shop(request):
     products =  Product.objects.all()
     return render(request, 'pets/shop.html', {'products': products})
@@ -57,28 +58,60 @@ def login_view(request):
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
-        role = request.POST.get("role")  # Lấy vai trò từ form
+        role = request.POST.get("role")
 
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
-            login(request, user)
+            if user.role == role:  
+                login(request, user)
 
-            # Điều hướng theo vai trò
-            if role == "customer":
-                return redirect("customer_dashboard")  
-            elif role == "admin":
-                return redirect("admin_dashboard") 
-            elif role == "vet":
-                return redirect("vet_dashboard")  
-            elif role == "vet":
-                return redirect("staff_dasboard") 
+                if role == "customer":
+                    return redirect("staff:tong_quan")  
+                elif role == "staff":
+                    return redirect("staff:tong_quan")
+                elif role == "doctor":
+                    return redirect("staff:tong_quan")
+                elif role == "admin":
+                    return redirect("staff:tong_quan")
+                else:
+                    messages.error(request, "Vai trò không hợp lệ.")
+                    return redirect("pets:login_page")
             else:
-                return redirect("home")  
+                messages.error(request, "Vai trò không khớp với tài khoản.")
+                return redirect("pets:login_page")
         else:
-            return render(request, "pets/login_page.html", {"error": "Sai thông tin đăng nhập"})
+            messages.error(request, "Tên đăng nhập hoặc mật khẩu không đúng.")
+            return redirect("pets:login_page")
+        
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        password_confirm = request.POST["password_confirm"]
+        role = request.POST["role"]
+
+        if password != password_confirm:
+            messages.error(request, "Mật khẩu xác nhận không khớp.")
+            return redirect("pets:register_views")
+
+        if CustomUser.objects.filter(username=username).exists():
+            messages.error(request, "Tên đăng nhập đã tồn tại.")
+            return redirect("pets:register_view")
+
+        if CustomUser.objects.filter(email=email).exists():
+            messages.error(request, "Email đã được sử dụng.")
+            return redirect("pets:register_view")
+
+        user = CustomUser.objects.create_user(username=username, email=email, password=password, role=role)
+        user.save()
+
+        messages.success(request, "Đăng ký thành công! Vui lòng đăng nhập.")
+        return redirect("pets:login_page")  
 
     return render(request, "pets/login_page.html")
+
 
 
 
