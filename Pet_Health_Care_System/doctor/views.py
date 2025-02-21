59e4a5e19ac98552b80a8e9ea2650bc7aca9049e
TranKhanhHuyen-lab <huyentk5897@ut.edu.vn>
2025-02-22 00:30:46 +0700

    Tính năng hiển thị lịch làm việc (lịch khám) từ database cho từng bác sĩ

--- Pet_Health_Care_System/doctor/views.py
+++ Pet_Health_Care_System/doctor/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib import messages
+from django.http import HttpResponse
+from pets.models import Appointment, CustomUser
 
 def tong_quan(request):
     context = {}
     return render(request,'doctor/tong_quan.html', context)
 
 def lich_lam_viec(request):
-    context = {}
-    return render(request,'doctor/lich_lam_viec.html', context)
+    if request.user.is_authenticated:
+        user_id = request.user.id
+        user = CustomUser.objects.get(id=user_id)
+
+        appointments = Appointment.objects.filter(doctor=user.full_name)
+    return render(request, 'doctor/lich_lam_viec.html', {"appointments": appointments})
 
 def ho_so_benh_an(request):
     context = {}
